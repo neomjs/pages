b2794f7b54164607d6d0ad11e23db69af26f1ee8
tobiu <tobiasuhlig78@gmail.com>
2019-12-01 17:59:31 +0100

    published neo.mjs (node module)

---
+++ node_modules/neo.mjs/src/worker/VDom.mjs
@@ -0,0 +1,46 @@
+import Neo       from '../Neo.mjs';
+import Base      from './Base.mjs';
+import * as core from '../core/_export.mjs';
+import Helper    from '../vdom/Helper.mjs';
+import NeoArray  from '../util/Array.mjs';
+import Style     from '../util/Style.mjs';
+
+/**
+ * The Vdom worker converts vdom templates into vnodes, as well as creating delta-updates.
+ * See the tutorials for further infos.
+ * @class Neo.worker.VDom
+ * @extends Neo.worker.Base
+ * @singleton
+ */
+class VDom extends Base {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.worker.VDom'
+         * @private
+         */
+        className: 'Neo.worker.VDom',
+        /**
+         * @member {String} ntype='vdom-worker'
+         * @private
+         */
+        ntype: 'vdom-worker',
+        /**
+         * @member {Boolean} singleton=true
+         * @private
+         */
+        singleton: true,
+        /**
+         * @member {String} workerId='vdom'
+         * @private
+         */
+        workerId: 'vdom'
+    }}
+}
+
+Neo.applyClassConfig(VDom);
+
+let instance = Neo.create(VDom);
+
+Neo.applyToGlobalNs(instance);
+
+export default instance;
