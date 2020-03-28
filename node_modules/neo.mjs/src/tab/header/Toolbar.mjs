85be259ffec63a1249284e4927d1a5b9269fd170
tobiu <tobiasuhlig78@gmail.com>
2020-03-28 13:57:51 +0100

    neo v1.0.74

---
+++ node_modules/neo.mjs/src/tab/header/Toolbar.mjs
@@ -0,0 +1,105 @@
+import {default as BaseToolbar} from '../../container/Toolbar.mjs';
+
+/**
+ * @class Neo.tab.header.Toolbar
+ * @extends Neo.container.Toolbar
+ */
+class Toolbar extends BaseToolbar {
+    static getConfig() {return {
+        /**
+         * @member {String} className='Neo.tab.header.Toolbar'
+         * @private
+         */
+        className: 'Neo.tab.header.Toolbar',
+        /**
+         * @member {String} ntype='tab-header-toolbar'
+         * @private
+         */
+        ntype: 'tab-header-toolbar',
+        /**
+         * @member {Array} cls=['neo-tab-header-toolbar','neo-toolbar']
+         */
+        cls: ['neo-tab-header-toolbar', 'neo-toolbar'],
+        /**
+         * @member {Boolean} useActiveTabIndicator_=true
+         */
+        useActiveTabIndicator_: true
+    }}
+
+    /**
+     * Triggered after the useActiveTabIndicator config got changed
+     * @param {Boolean} value
+     * @param {Boolean} oldValue
+     * @private
+     */
+    afterSetUseActiveTabIndicator(value, oldValue) {
+        if (Neo.isDefined(oldValue)) {
+            let me   = this,
+                vdom = me.vdom;
+
+            me.items.forEach(item => {
+                // silent updates
+                item._useActiveTabIndicator = value;
+                item.updateUseActiveTabIndicator(true);
+            });
+
+            me.vdom = vdom;
+        }
+    }
+
+    /**
+     *
+     * @private
+     */
+    createItems() {
+        let me       = this,
+            defaults = me.itemDefaults || {};
+
+        defaults.useActiveTabIndicator = me.useActiveTabIndicator;
+        me.itemDefaults = defaults;
+
+        super.createItems();
+    }
+
+    /**
+     * Returns the layout config matching to the dock position
+     * @returns {Object} layoutConfig
+     * @private
+     */
+    getLayoutConfig() {
+        let layoutConfig;
+
+        switch(this.dock) {
+            case 'bottom':
+            case 'top':
+                layoutConfig = {
+                    ntype: 'hbox',
+                    align: 'center',
+                    pack : 'start'
+                };
+                break;
+            case 'left':
+                layoutConfig = {
+                    ntype    : 'vbox',
+                    align    : 'center',
+                    direction: 'column-reverse',
+                    pack     : 'end'
+                };
+                break;
+            case 'right':
+                layoutConfig = {
+                    ntype    : 'vbox',
+                    align    : 'center',
+                    direction: 'column',
+                    pack     : 'start'
+                };
+                break;
+        }
+
+        return layoutConfig;
+    }
+}
+
+Neo.applyClassConfig(Toolbar);
+
+export {Toolbar as default};
